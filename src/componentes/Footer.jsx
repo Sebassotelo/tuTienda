b08a81d6b58fa3f67e9ed6479108173ba71e6038
Sebas Sotelo <sebassotelo97@gmail.com>
2023-04-21 04:54:37 -0300

    footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -16,15 +16,18 @@ function Footer() {
           <h3>MEDIOS DE PAGO</h3>
           <p>MercadoPago y Efectivo</p>
           <h3>MEDIOS DE ENVIOS</h3>
-          <p>Pickup y Motomandado</p>
+          <p>⚡P I C K U P ⚡ y Motomandado</p>
         </div>
         <div className={style.contacto}>
           <h3>NUESTRAS REDES SOCIALES</h3>
           <div className={style.icon__container}>
-            <a href="">
+            <a href="https://www.instagram.com/sras.medias/" target={"_blank"}>
               <AiOutlineInstagram className={style.icon} />
             </a>
-            <a href="">
+            <a
+              href="https://api.whatsapp.com/send/?phone=5493624148393&text=Pega+aqui+tu+pedido%21&type=phone_number&app_absent=0"
+              target={"_blank"}
+            >
               <AiOutlineWhatsApp className={style.icon} />
             </a>
           </div>
@@ -35,11 +38,16 @@ function Footer() {
           </div>
           <div className={style.contacto__item}>
             <AiOutlinePhone />
-            <a href="">3624148393</a>
+            <p>3624148393</p>
           </div>
           <div className={style.contacto__item}>
             <FaMapMarkerAlt />{" "}
-            <a href="">Catamarca 1326. Corrientes, Argentina</a>
+            <a
+              href="https://www.google.com/maps/place/Catamarca+1326,+W3400CEU+W3400CEU,+Corrientes/@-27.4726349,-58.8335555,19z/data=!4m6!3m5!1s0x94456c9f58a152cd:0x7af34e30d000883e!8m2!3d-27.4726694!4d-58.8336816!16s%2Fg%2F11fssw2pdc"
+              target={"_blank"}
+            >
+              Catamarca 1326. Corrientes, Argentina
+            </a>
           </div>
         </div>
       </div>
